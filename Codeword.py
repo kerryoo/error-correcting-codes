7d7ff3d950e52daecb4f1e7ad7d747f7491885aa
kerryoo <kerr.yen.yoo@vanderbilt.edu>
2020-09-22 15:36:54 -0500

    updated readme and added distance function

--- Codeword.py
+++ Codeword.py
@@ -4,6 +4,28 @@
 Created on Tue Sep 22 13:53:34 2020
 
 @author: kerryoo
+
+You can only initialize Codewords from a string or int containing only 0's and
+1's or a List containing single digit integers (again only 0's and 1's).
+
+If you try to convert it to a string or print it, it'll be formatted like
+a regular integer. For example, while the class saves 1001 as [1, 0, 0, 1],
+it will be converted to 1001.
+
+If you use the len function, for example len(codewordA), the length of the codeword
+will be returned.
+
+If you use the index function, for example codeword[3], the digit at that index
+will be returned.
+
+You can add two codewords together. I've added some protection against adding
+things that aren't explicitly defined as codewords. You should be able to add
+properly formated integers, strings, and lists to codewords without concern.
+It is also not a concern of adding codewords of different lengths. The shorter length
+codeword will have 0's attached to the front.
+
+The distance function is built primarily from the addition function, so the same
+holds true for this function.
 """
 
 class Codeword:
@@ -64,5 +86,8 @@ class Codeword:
                 result.append(1)
         return Codeword(result)
             
-                
+    def distance(self, rhsWord):
+        wordSum = self + rhsWord
+        return wordSum.__weight
+        
         
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # error-correcting-codes
 Sometimes the homework takes ages to complete by hand. Instead, we should write code to automate it for us.
+
+Currently, only the Codeword class exists.
+The functions that have been implemented are getting the length, creating a
+codeword, getting distance between two codewordsand adding two codewords.
+
+Things to be done:
+Create a class for Codeword matrices
